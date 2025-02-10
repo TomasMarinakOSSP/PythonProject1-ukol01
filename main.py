a33fd85fea96ef58bd5ad4d40f841ccc57f0822a
TomasMarinakOSSP <tomas.marinak@student.ossp.cz>
2025-02-10 14:12:23 +0100

    index.html změna extnduje base html a je v block content a v main jsem to taky zmenil a v base taky

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def base():
-    return render_template('base.html')
 
-@app.route('/index')
+@app.route('/')
 def home():
     return render_template('index.html')
 
