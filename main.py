1f02c069eb8d76d3fd254d189b972a5cbe51bca0
TomasMarinakOSSP <tomas.marinak@student.ossp.cz>
2025-02-10 14:07:32 +0100

    main.py stažení flasku a přidání routu na jednotlivy soubory

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/abc')
+def abc():
+    return render_template('abc.html')
+
+
+@app.route('/azb')
+def azb():
+    return render_template('azb.html')
+
+
+@app.route('/alpha')
+def alpha():
+    return render_template('alfa.html')
+
+
+@app.route('/heb')
+def heb():
+    return render_template('heb.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
