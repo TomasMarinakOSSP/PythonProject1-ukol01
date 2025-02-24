cf1ff2ba81ba0b3956a01d241960a3177d3ecab1
TomasMarinakOSSP <tomas.marinak@student.ossp.cz>
2025-02-24 10:17:59 +0100

    commit s flash message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "dev"
 
 
 @app.route('/')
@@ -8,24 +9,18 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/abc')
-def abc():
-    return render_template('abc.html')
-
-
-@app.route('/azb')
-def azb():
-    return render_template('azb.html')
-
-
-@app.route('/alfa')
-def alpha():
-    return render_template('alfa.html')
-
-
-@app.route('/heb')
-def heb():
-    return render_template('heb.html')
+@app.route('/login', methods=['GET', 'POST'])
+def link():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        radio = request.form['radio']
+        if username == 'pokuston' and password == 'heslo':
+            flash('Přihlášení bylo úspěšné!', 'success')
+        else:
+            flash('Přihlášení selhalo! Zkuste to znovu.', 'warning')
+        return render_template('index.html', username=username, password=password, radio=radio)
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
